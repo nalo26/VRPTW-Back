fa4e4624dabd67d3a37c7d19e30f5124c3162f27
Galding <nathan.peyronnet@gmail.com>
2023-03-25 12:56:58 +0100

    initial commit

---
+++ src/main/java/com/polypote/vrptwback/VrptwBackApplication.java
@@ -0,0 +1,13 @@
+package com.polypote.vrptwback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrptwBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrptwBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/polypote/vrptwback/VrptwBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polypote.vrptwback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrptwBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
