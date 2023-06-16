26be7405b9d98a1ed5c6dcd054b4146666d551a7
nalo26 <nath.v26@gmail.com>
2023-06-16 18:51:49 +0200

    Interrupt all workers on new one appears

--- src/main/java/com/polypote/vrptwback/service/TabouAlgorithmService.java
+++ src/main/java/com/polypote/vrptwback/service/TabouAlgorithmService.java
@@ -1,6 +1,7 @@
 package com.polypote.vrptwback.service;
 
 import com.polypote.vrptwback.algorithms.TabouSearchAlgorithm;
+import com.polypote.vrptwback.client.DataSender;
 import com.polypote.vrptwback.generator.RandomSolutionGenerator;
 import com.polypote.vrptwback.model.Root;
 import com.polypote.vrptwback.model.Solution;
@@ -24,5 +25,6 @@ public class TabouAlgorithmService {
         Solution randomSolution = randomSolutionGenerator.generate(root);
         TabouSearchAlgorithm tabouSearchAlgorithm = new TabouSearchAlgorithm(nbIter, operators, tabouSize);
         Solution result = tabouSearchAlgorithm.resolve(randomSolution);
+        DataSender.sendSolutionToFront(result);
     }
 }
